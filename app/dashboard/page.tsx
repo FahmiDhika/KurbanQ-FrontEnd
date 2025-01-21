4f0d67fc9e1156e4651c9d48ab5b66e979b9faf2
FahmiDhika <fahmidhika810@gmail.com>
2025-01-21 14:05:10 +0700

    filtering jenis hewan di dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import { Item } from "@/components/keunggulan";
+import { FilteringButton } from "@/components/button/filtering";
 
 import mainImage from "@/public/landingImage.svg";
 import domba from "@/public/domba.svg";
@@ -16,8 +17,14 @@ import keunggulan1 from "@/public/keunggulan1.svg";
 import keunggulan2 from "@/public/keunggulan2.svg";
 import keunggulan3 from "@/public/keunggulan3.svg";
 
+import logoFilterDomba from "@/public/logoDomba.png";
+import logoFilterSapi from "@/public/logoSapi.png";
+import logoFilterKambing from "@/public/logoKambing.png";
+
 export default function Dashboard() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({delay: 2000, stopOnInteraction: false})]);
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 2000, stopOnInteraction: false }),
+  ]);
 
   return (
     // image carousel
@@ -58,9 +65,42 @@ export default function Dashboard() {
       </div>
 
       <div className="flex w-full justify-evenly">
-        <Image src={domba} alt="" width={500}></Image>
-        <Image src={sapi} alt="" width={500}></Image>
-        <Image src={kambing} alt="" width={500}></Image>
+        <Image src={domba} alt="" width={400}></Image>
+        <Image src={sapi} alt="" width={400}></Image>
+        <Image src={kambing} alt="" width={400}></Image>
+      </div>
+
+      <h1 className="pl-14 w-full text-4xl font-bold my-24">Hewan Kurban</h1>
+
+      <div className="w-full pl-14 flex gap-10 justify-start">
+        <FilteringButton
+          label="Domba"
+          classname=""
+          icon={logoFilterDomba}
+          onClick={() => {
+            window.location.href = "";
+          }}
+        />
+        <FilteringButton
+          label="Sapi"
+          classname="bg-white"
+          icon={logoFilterSapi}
+          onClick={() => {
+            window.location.href = "";
+          }}
+        />
+        <FilteringButton
+          label="Kambing"
+          classname="bg-white"
+          icon={logoFilterKambing}
+          onClick={() => {
+            window.location.href = "";
+          }}
+        />
+      </div>
+
+      <div className="">
+
       </div>
     </>
   );
